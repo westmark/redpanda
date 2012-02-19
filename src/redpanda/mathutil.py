93854f1e6ef5bd2260171047826d40a06a1088e5
Fredrik Westmark <fredrik.westmark@gmail.com>
2012-02-19 12:48:46 +0100

    find_rotation_quat can now make multiple rotations

--- src/redpanda/mathutil.py
+++ src/redpanda/mathutil.py
@@ -1,24 +1,33 @@
 # -*- coding: UTF-8 -*-
 
-
 from panda3d.core import Quat, Vec3
 
 
-def find_rotation_quat(v1, v2):
-  if v1.almost_equal(v2):
-    return Quat(1, 0, 0, 0)
-  elif (v1 * -1).almost_equal(v2):
-    v, smallest = Vec3(1, 0, 0), abs(v1[0])
-    if abs(v[1]) < smallest:
-      v = Vec3(0, 1, 0)
-      smallest = abs(v[1])
-    if abs(v[2]) < smallest:
-      v = Vec3(0, 0, 1)
+def find_rotation_quat(*args):
+  v1 = Vec3(args[0])
+  q = Quat(1, 0, 0, 0)
+  vectors = args[1:]
 
-    return Quat(0, v1.cross(v))
+  for v2 in vectors:
+    v2 = Vec3(v2)
+    if v1.almost_equal(v2):
+      continue
+    elif (v1 * -1).almost_equal(v2):
+      v, smallest = Vec3(1, 0, 0), abs(v1[0])
+      if abs(v[1]) < smallest:
+        v = Vec3(0, 1, 0)
+        smallest = abs(v[1])
+      if abs(v[2]) < smallest:
+        v = Vec3(0, 0, 1)
 
-  v = v1 + v2
-  v.normalize()
-  angle = v.dot(v2)
-  axis = v.cross(v2)
-  return Quat(angle, axis)
+      q = Quat(0, v1.cross(v)) * q
+    else:
+      v = v1 + v2
+      v.normalize()
+      angle = v.dot(v2)
+      axis = v.cross(v2)
+      q = Quat(angle, axis) * q
+
+    v1 = v2
+
+  return q
